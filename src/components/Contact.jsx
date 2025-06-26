86b20562d0caa3a9184443694bf34d52dac37fbe
samsundarsingh-22 <enoch.sam444@gmail.com>
2025-06-26 20:40:24 +0530

    Update Contact page: remove LinkedIn

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Contact() {
   return (
-    <div>
-      <h2>Contact</h2>
-      <p>Email: samsundarsingh@iisc.ac.in</p>
-      <p>Email: enoch.sam444@gmail.com</p>
+    <div className="p-6 text-center space-y-4">
+      <h2 className="text-3xl font-bold text-blue-900">Contact</h2>
+      <p>Email: <a href="mailto:samsundarsingh@iisc.ac.in" className="text-blue-700 hover:underline">samsundarsingh@iisc.ac.in</a></p>
+      <p>Alternate Email: <a href="mailto:enoch.sam444@gmail.com" className="text-blue-700 hover:underline">enoch.sam444@gmail.com</a></p>
     </div>
   );
 }
