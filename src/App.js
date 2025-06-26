fb31d2c4ee96476a5aa33e54585ddb4fe6313de4
samsundarsingh-22 <enoch.sam444@gmail.com>
2025-06-26 19:58:36 +0530

    Fix image path for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Teaching from "./components/Teaching";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/samsundarsingh-22.github.io">
       <nav className="bg-white shadow p-4 flex flex-wrap justify-center space-x-6">
         <Link to="/" className="text-blue-600 font-semibold hover:underline">Home</Link>
         <Link to="/about" className="text-blue-600 font-semibold hover:underline">About</Link>
