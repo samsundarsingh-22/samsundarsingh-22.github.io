5c631cf30cb4cc1b2ca37d5ea720566b2f73efda
samsundarsingh-22 <enoch.sam444@gmail.com>
2025-06-26 19:36:40 +0530

    Fix: Remove basename to render routes correctly

--- src/App.js
+++ src/App.js
@@ -8,30 +8,26 @@ import Teaching from "./components/Teaching";
 
 function App() {
   return (
-    <Router basename="/samsundarsingh-22.github.io">
-      <div className="flex flex-col min-h-screen">
-        <nav className="bg-white shadow p-4 flex flex-wrap justify-center space-x-6">
-          <Link to="/" className="text-blue-600 font-semibold hover:underline">Home</Link>
-          <Link to="/about" className="text-blue-600 font-semibold hover:underline">About</Link>
-          <Link to="/research" className="text-blue-600 font-semibold hover:underline">Research</Link>
-          <Link to="/teaching" className="text-blue-600 font-semibold hover:underline">Teaching</Link>
-          <Link to="/contact" className="text-blue-600 font-semibold hover:underline">Contact</Link>
-        </nav>
+    <Router>
+      <nav className="bg-white shadow p-4 flex flex-wrap justify-center space-x-6">
+        <Link to="/" className="text-blue-600 font-semibold hover:underline">Home</Link>
+        <Link to="/about" className="text-blue-600 font-semibold hover:underline">About</Link>
+        <Link to="/research" className="text-blue-600 font-semibold hover:underline">Research</Link>
+        <Link to="/teaching" className="text-blue-600 font-semibold hover:underline">Teaching</Link>
+        <Link to="/contact" className="text-blue-600 font-semibold hover:underline">Contact</Link>
+      </nav>
 
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/research" element={<Research />} />
-            <Route path="/teaching" element={<Teaching />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/research" element={<Research />} />
+        <Route path="/teaching" element={<Teaching />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
 
-        <footer className="bg-gray-100 text-center text-gray-600 py-4 mt-8">
-          © {new Date().getFullYear()} Sam Sundar Singh. All rights reserved.
-        </footer>
-      </div>
+      <footer className="bg-gray-100 text-center text-gray-600 py-4 mt-12 shadow-inner">
+        © {new Date().getFullYear()} Sam Sundar Singh. All rights reserved.
+      </footer>
     </Router>
   );
 }
